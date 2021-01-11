a75af5274bee28bfd46d89b28ed71de32d6d820b
SNarboni <sebastien.narboni@hotmail.fr>
2021-01-11 14:49:09 +0100

    exo 1 jusqu'a 18 fini

--- exercice17/js/main.js
+++ exercice17/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $("button").click(function () {
         $("#text").show("Konexio!")
-        $("#exercise").append('<h2>Display : none</h2>');
+        $("#exercise").append('<h2>Display : block</h2>');
     })
 })
---
+++ exercice18/js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $("button").click(function () {
+        $("#text").hide("Konexio!")
+        $("#exercise").append('<h2>Display : none</h2>');
+    })
+})
